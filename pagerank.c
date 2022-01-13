111c537561a958995dc2445ee00127bb075832b3
Simon <simi.pra@gmail.com>
2022-01-13 17:17:06 +0100

    proof of concept

--- pagerank.c
+++ pagerank.c
@@ -7,18 +7,19 @@ void pagerank();
 
 int main(int argc, char const *argv[])
 {
-    t_matrice matrice1 = lire_matrice();
-    t_matrice matrice2 = matrice_uniforme(5,1,(float)1/5);
-    t_matrice produit = produit_matriciel(matrice1,matrice2);
-    afficher_matrice(produit);
-    vider_matrice(matrice1);
-    vider_matrice(matrice2);
-    vider_matrice(produit);
+    pagerank(5);
     return 0;
 }
 
 void pagerank(int n){
+    t_matrice m = lire_matrice();
     t_matrice r = matrice_uniforme(n, 1, (float)1/n);
+    t_matrice prod;
+    for(int i = 0; i < 100; i++){
+        prod = produit_matriciel(m,r);
+        vider_matrice(r);
+        r = prod;
+    }
     afficher_matrice(r);
     vider_matrice(r);
 }
